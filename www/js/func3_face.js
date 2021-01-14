6215c20a968d7e13d3bfe2ad088da2d9fc81bbdc
du1209 <daiki.uehara@unisys.co.jp>
2021-01-14 18:17:10 +0900

    aa

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -59,7 +59,7 @@ var uriBase = "https://" + area + ".api.cognitive.microsoft.com/face/v1.0/detect
 var params = {
     "returnFaceId": "true",
     "returnFaceLandmarks": "false",
-    "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+    "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,mask,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
 };
 
 // Perform the REST API call.

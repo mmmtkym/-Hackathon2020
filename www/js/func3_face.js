49e04ffbe27ee8ad6711c6d25c7dc079f3a6bc5c
du1209 <daiki.uehara@unisys.co.jp>
2021-01-14 15:21:44 +0900

    v1.6

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -45,23 +45,9 @@ function ohayou3() {
     }
 
 
-var age = 0;                // 年齢'
-var gender = 0;             // 性別 （male/female)
-var emoSmile = 0;           // 笑顔'
-var emoAnger = 0;           // 怒り'
-var emoContempt = 0;        // 不面目
-var emoDisgust = 0;         // 嫌気
-var emoFear = 0;            // 恐怖
-var emoHappiness = 0;       // 幸福
-var emoNeutral = 0;         // 無感動
-var emoSadness = 0;         // 悲しみ
-var emoSuprise = 0;         // 感嘆
-var makeEye = 0;            // 化粧-目 (true/false)
-var makeLip = 0;            // 化粧-唇 (true/false)
-var hairBald = 0;           // 髪の量
-var hairInvisible = 0;      // スキンヘッド (true/false)
-var glasses = 0;            // メガネ (NoGlasses/ReadingGlasses/Sunglasses/SwimmingGoggles)
-
+var emo=[7];
+var max;
+var emotio=0;
 
 function azureFunc(file){
 var area = "westus"; 
@@ -100,52 +86,59 @@ $.ajax({
     // 写真に人が写っていれば、その人数がlengthに入ります
     if( data.length > 0 ){
        // 複数人の場合data["1"]、["2"]となっていきます
-       // 年齢
-       age = data["0"].faceAttributes.age;  
-       // 性別 （male/female)
-       gender = data["0"].faceAttributes.gender; 
        // 数理のデータは100倍して0～100の数値にします。（使いやすいから）
-       // 笑顔
-       emoSmile = Math.floor(data["0"].faceAttributes.smile * 100);
        // 怒り
-       emoAnger = Math.floor(data["0"].faceAttributes.emotion.anger * 100); 
+       emo[0] = Math.floor(data["0"].faceAttributes.emotion.anger * 100); 
        // 不面目
-       emoContempt = Math.floor(data["0"].faceAttributes.emotion.contempt * 100); 
+       emo[1] = Math.floor(data["0"].faceAttributes.emotion.contempt * 100); 
        // 嫌気
-       emoDisgust = Math.floor(data["0"].faceAttributes.emotion.disgust * 100);                    
+       emo[2] = Math.floor(data["0"].faceAttributes.emotion.disgust * 100);                    
        // 恐怖
-       emoFear = Math.floor(data["0"].faceAttributes.emotion.fear * 100);
+       emo[3] = Math.floor(data["0"].faceAttributes.emotion.fear * 100);
        // 幸福
-       emoHappiness = Math.floor(data["0"].faceAttributes.emotion.happiness * 100); 
+       emo[4] = Math.floor(data["0"].faceAttributes.emotion.happiness * 100); 
        // 無感動
-       emoNeutral = Math.floor(data["0"].faceAttributes.emotion.neutral * 100);
+       emo[5] = Math.floor(data["0"].faceAttributes.emotion.neutral * 100);
        // 悲しみ
-       emoSadness = Math.floor(data["0"].faceAttributes.emotion.sadness * 100);      
+       emo[6] = Math.floor(data["0"].faceAttributes.emotion.sadness * 100);      
        // 感嘆
-       emoSuprise = Math.floor(data["0"].faceAttributes.emotion.surprise * 100);           
-       // 化粧-目 (true/false)
-       makeEye = data["0"].faceAttributes.makeup.eyeMakeup; 
-       // 化粧-唇 (true/false)
-       makeLip = data["0"].faceAttributes.makeup.lipMakeup;     
-       // 髪の量
-       hairBald = data["0"].faceAttributes.hair.bald; 
-       // スキンヘッド (true/false)
-       hairInvisible = data["0"].faceAttributes.hair.invisible;
-       // メガネ (NoGlasses/ReadingGlasses/Sunglasses/SwimmingGoggles)
-       glasses = data["0"].faceAttributes.glasses; 
+       emo[7] = Math.floor(data["0"].faceAttributes.emotion.surprise * 100); 
+       //露出度
+
+
     }
+    max=emo[0];
     // FaceAPIから取得した情報を使います
-    alert("あなたは" + age + "歳です");
-    if( emoSmile > 50 )
-    {
-        alert("とても笑顔です");
-    }
-    else if( emoSmile > 0 ) {
-        alert("笑顔です");
-    }
-    else {
-        alert("笑顔じゃありません");
-    }
+    for(var i=1;i<7;i++){
+if(max<emo[i]){
+  max=emo[i];
+  emotion=i;
+}
+}
+if(i==0){
+  alert("怒り");
+}
+ if(i==1){
+  alert("不面目");
+}
+if(i==2){
+  alert("嫌気");
+}
+if(i==3){
+  alert("恐怖");
+}
+if(i==4){
+  alert("幸福");
+}
+if(i==5){
+  alert("無感動");
+}
+if(i==6){
+  alert("悲しみ");
+}
+if(i==7){
+  alert("感嘆");
+}
 })
 //失敗時！
 .fail(function(jqXHR, textStatus, errorThrown) {

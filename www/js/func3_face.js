bc6b36533f9aba655cb946df93ee9a816397c6f1
du1209 <daiki.uehara@unisys.co.jp>
2021-01-15 11:44:00 +0900

    aaa

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -107,7 +107,7 @@ $.ajax({
        emo[7] = Math.floor(data["0"].faceAttributes.emotion.surprise * 100); 
        emo[8]=data["0"].faceAttributes.occlusion.mouthOccluded;
 
-       mask = data["0"].faceAttributes.accessories.mask;
+       mask = data["0"].faceAttributes.accessories.type.mask.confidence;
        y[0]= Math.floor(data["0"].faceLandmarks.noseLeftAlarOutTip.y);
       //x[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.x);
      // y[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.y);

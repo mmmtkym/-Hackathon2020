f800497356bb2ef2b5f25d41553cacbab8f03373
du1209 <daiki.uehara@unisys.co.jp>
2021-01-15 12:24:08 +0900

    aaa

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -109,15 +109,16 @@ $.ajax({
 
        x[0]= Math.floor(data["0"].faceLandmarks.noseTip.x);
        y[0]= Math.floor(data["0"].faceLandmarks.noseTip.y);
-      //x[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.x);
-     // y[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.y);
+      x[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.x);
+      y[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.y);
 
 
     
     }
     alert(x[0]);
     alert(y[0]);
-    
+     alert(x[1]);
+    alert(y[1]);
     max=emo[0];
     // FaceAPIから取得した情報を使います
   

89fc64d5258b94aa092bf652b75971bbacc9ea3d
du1209 <daiki.uehara@unisys.co.jp>
2021-01-15 12:19:09 +0900

    aaa

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -121,7 +121,7 @@ $.ajax({
     max=emo[0];
     // FaceAPIから取得した情報を使います
   
-   if(y[0]>0 && emo[8]==false){
+   if(y[0]>0 && emo[8]==true){
          for(var i=1;i<7;i++){
 if(max<emo[i]){
   max=emo[i];

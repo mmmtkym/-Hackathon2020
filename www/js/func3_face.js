29491891747043b057df5b9063de87017483a073
du1209 <daiki.uehara@unisys.co.jp>
2021-01-15 11:07:01 +0900

    aaa

--- www/js/func3_face.js
+++ www/js/func3_face.js
@@ -50,7 +50,7 @@ var max;
 var emotion=0;
 var x=[2];
 var y=[2];
-
+var mask;
 function azureFunc(file){
 var area = "westus"; 
 var subscriptionKey = "0a50c6d56ddb41c98f952861abe8c050" 
@@ -107,7 +107,7 @@ $.ajax({
        emo[7] = Math.floor(data["0"].faceAttributes.emotion.surprise * 100); 
        emo[8]=data["0"].faceAttributes.occlusion.mouthOccluded;
 
-       var mask = data["0"].faceAttributes.accessories;
+       mask = data["0"].faceAttributes.accessories.type;
        y[0]= Math.floor(data["0"].faceLandmarks.noseLeftAlarOutTip.y);
       //x[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.x);
      // y[1]= Math.floor(data["0"].faceLandmarks.underLipBottom.y);
